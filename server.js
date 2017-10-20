e2f8a6004579ea15cf909c187a1173f7b4f41b0d
Keith Dahlby <keith@newbo.co>
2017-10-20 09:57:50 -0500

    Add basic static server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+require('dotenv').config()
+
+const app = express()
+
+app.use(morgan('common'))
+app.use(express.static(`${__dirname}/build`))
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+
+app.listen(process.env.PORT, () => {
+  console.log('__SERVER_RUNNING__', process.env.PORT)
+})
